38ffe2d3fc91694308a554eb5f1f14d482bc50f4
koudex <zishin.dev@gmail.com>
2025-08-24 11:27:42 +0800

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -2,136 +2,136 @@ const playlist = [
     {
         title: "Beyond Journey",
         artist: "briancjb",
-        src: "music/Beyond Journey.mp3"
+        src: "/myspace/music/Beyond Journey.mp3"
     },
     {
         title: "No. 1 Party Anthem",
         artist: "Arctic Monkeys",
-        src: "music/No. 1 Party Anthem.mp3"
+        src: "/myspace/music/No. 1 Party Anthem.mp3"
     },
     {
         title: "I Wanna Be Yours",
         artist: "Arctic Monkeys",
-        src: "music/I Wanna Be Yours.mp3"
+        src: "/myspace/music/I Wanna Be Yours.mp3"
     },
     {
         title: "Chibi",
         artist: "briancjb",
-        src: "music/Chibi.mp3"
+        src: "/myspace/music/Chibi.mp3"
     },
     {
         title: "Copium",
         artist: "briancjb",
-        src: "music/Copium.mp3"
+        src: "/myspace/music/Copium.mp3"
     },
     {
         title: "Domino",
         artist: "briancjb",
-        src: "music/Domino.mp3"
+        src: "/myspace/music/Domino.mp3"
     },
     {
         title: "Fever Dream",
         artist: "briancjb",
-        src: "music/Fever Dream.mp3"
+        src: "/myspace/music/Fever Dream.mp3"
     },
     {
         title: "Frogs",
         artist: "briancjb",
-        src: "music/Frogs.mp3"
+        src: "/myspace/music/Frogs.mp3"
     },
     {
         title: "Gizmo",
         artist: "briancjb",
-        src: "music/Gizmo.mp3"
+        src: "/myspace/music/Gizmo.mp3"
     },
     {
         title: "Go Fish",
         artist: "briancjb, RJ Pasin",
-        src: "music/Go Fish.mp3"
+        src: "/myspace/music/Go Fish.mp3"
     },
     {
         title: "Jakarta",
         artist: "G Sounds",
-        src: "music/Jakarta.mp3"
+        src: "/myspace/music/Jakarta.mp3"
     },
     {
         title: "Laid-Back",
         artist: "briancjb",
-        src: "music/Laid-Back.mp3"
+        src: "/myspace/music/Laid-Back.mp3"
     },
     {
         title: "Link Start",
         artist: "briancjb",
-        src: "music/Link Start.mp3"
+        src: "/myspace/music/Link Start.mp3"
     },
     {
         title: "Macaroni",
         artist: "briancjb",
-        src: "music/Macaroni.mp3"
+        src: "/myspace/music/Macaroni.mp3"
     },
     {
         title: "Marbles",
-        artist: "MINLEE, brianjcb,FAIELO",
-        src: "music/Marbles.mp3"
+        artist: "MINLEE, brianjcb, FAIELO",
+        src: "/myspace/music/Marbles.mp3"
     },
     {
         title: "Mimikyu",
         artist: "briancjb",
-        src: "music/Mimikyu.mp3"
+        src: "/myspace/music/Mimikyu.mp3"
     },
     {
         title: "Pixels",
         artist: "briancjb",
-        src: "music/Pixels.mp3"
+        src: "/myspace/music/Pixels.mp3"
     },
     {
         title: "Pixels Pt. 2",
         artist: "Zachz Winner, briancjb",
-        src: "music/Pixels Pt. 2.mp3"
+        src: "/myspace/music/Pixels Pt. 2.mp3"
     },
     {
         title: "Shimmer",
         artist: "briancjb",
-        src: "music/Shimmer.mp3"
+        src: "/myspace/music/Shimmer.mp3"
     },
     {
         title: "Skill Issue",
         artist: "briancjb",
-        src: "music/Skill Issue.mp3"
+        src: "/myspace/music/Skill Issue.mp3"
     },
     {
         title: "Stargazer",
         artist: "briancjb",
-        src: "music/Stargazer.mp3"
+        src: "/myspace/music/Stargazer.mp3"
     },
     {
         title: "Vinland",
         artist: "briancjb",
-        src: "music/Vinland.mp3"
+        src: "/myspace/music/Vinland.mp3"
     },
     {
         title: "Wonder",
         artist: "briancjb",
-        src: "music/Wonder.mp3"
+        src: "/myspace/music/Wonder.mp3"
     },
     {
         title: "Birdo",
         artist: "G Sounds",
-        src: "music/Birdo.mp3"
+        src: "/myspace/music/Birdo.mp3"
     },
     {
         title: "Bytes",
         artist: "briancjb",
-        src: "music/Bytes.mp3"
+        src: "/myspace/music/Bytes.mp3"
     },
     {
         title: "Multo",
         artist: "Cup of Joe",
-        src: "music/multo.mp3"
+        src: "/myspace/music/multo.mp3"
     },
     {
         title: "Calm Mind",
         artist: "briancjb",
-        src: "music/Calm Mind.mp3"
-    },
+        src: "/myspace/music/Calm Mind.mp3"
+    }
 ];
