5b3656c3b1e9103c98fb424c49750bc5e08e6eff
koudex <zishin.dev@gmail.com>
2025-08-24 09:31:31 +0800

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,127 @@
+const playlist = [
+    {
+        title: "Beyond Journey",
+        artist: "briancjb",
+        src: "music/Beyond Journey.mp3"
+    },
+    {
+        title: "No. 1 Party Anthem",
+        artist: "Arctic Monkeys",
+        src: "music/No. 1 Party Anthem.mp3"
+    },
+    {
+        title: "I Wanna Be Yours",
+        artist: "Arctic Monkeys",
+        src: "music/I Wanna Be Yours.mp3"
+    },
+    {
+        title: "Chibi",
+        artist: "briancjb",
+        src: "music/Chibi.mp3"
+    },
+    {
+        title: "Copium",
+        artist: "briancjb",
+        src: "music/Copium.mp3"
+    },
+    {
+        title: "Domino",
+        artist: "briancjb",
+        src: "music/Domino.mp3"
+    },
+    {
+        title: "Fever Dream",
+        artist: "briancjb",
+        src: "music/Fever Dream.mp3"
+    },
+    {
+        title: "Frogs",
+        artist: "briancjb",
+        src: "music/Frogs.mp3"
+    },
+    {
+        title: "Gizmo",
+        artist: "briancjb",
+        src: "music/Gizmo.mp3"
+    },
+    {
+        title: "Go Fish",
+        artist: "briancjb, RJ Pasin",
+        src: "music/Go Fish.mp3"
+    },
+    {
+        title: "Jakarta",
+        artist: "G Sounds",
+        src: "music/Jakarta.mp3"
+    },
+    {
+        title: "Laid-Back",
+        artist: "briancjb",
+        src: "music/Laid-Back.mp3"
+    },
+    {
+        title: "Link Start",
+        artist: "briancjb",
+        src: "music/Link Start.mp3"
+    },
+    {
+        title: "Macaroni",
+        artist: "briancjb",
+        src: "music/Macaroni.mp3"
+    },
+    {
+        title: "Marbles",
+        artist: "MINLEE, brianjcb,FAIELO",
+        src: "music/Marbles.mp3"
+    },
+    {
+        title: "Mimikyu",
+        artist: "briancjb",
+        src: "music/Mimikyu.mp3"
+    },
+    {
+        title: "Pixels",
+        artist: "briancjb",
+        src: "music/Pixels.mp3"
+    },
+    {
+        title: "Pixels Pt. 2",
+        artist: "Zachz Winner, briancjb",
+        src: "music/Pixels Pt. 2.mp3"
+    },
+    {
+        title: "Shimmer",
+        artist: "briancjb",
+        src: "music/Shimmer.mp3"
+    },
+    {
+        title: "Skill Issue",
+        artist: "briancjb",
+        src: "music/Skill Issue.mp3"
+    },
+    {
+        title: "Stargazer",
+        artist: "briancjb",
+        src: "music/Stargazer.mp3"
+    },
+    {
+        title: "Vinland",
+        artist: "briancjb",
+        src: "music/Vinland.mp3"
+    },
+    {
+        title: "Wonder",
+        artist: "briancjb",
+        src: "music/Wonder.mp3"
+    },
+    {
+        title: "Birdo",
+        artist: "G Sounds",
+        src: "music/Birdo.mp3"
+    },
+    {
+        title: "Bytes",
+        artist: "briancjb",
+        src: "music/Bytes.mp3"
+    },
+];
